758ed6bcede8006f6f974058ec21dc5fc5c6119a
Eduardo AG <edag4000@gmail.com>
2022-03-10 11:39:32 -0600

    Agregar cambio de color como fondo de texto de resultado

--- javascript/script.js
+++ javascript/script.js
@@ -13,24 +13,31 @@ function calc_IMC() {
 
 	if (isNaN(imc) || !isFinite(imc)) {
 		resultado += ".\nPor favor introduzca valore validos."
+		document.getElementById("res").style.backgroundColor = "gray";
 
 	} else if (imc < 18.6) {
 		resultado += ".\nUsted tiene peso bajo."
+		document.getElementById("res").style.backgroundColor = "blue";
 
 	} else if (imc < 25) {
 		resultado += ".\nUsted tiene peso saludable."
+		document.getElementById("res").style.backgroundColor = "green";
 
 	} else if (imc < 30) {
 		resultado += ".\nUsted tiene sobrepeso."
+		document.getElementById("res").style.backgroundColor = "yellow";
 
 	} else if (imc < 35) {
 		resultado += ".\nUsted tiene obesidad de grado 1."
+		document.getElementById("res").style.backgroundColor = "orange";
 
 	} else if (imc < 40) {
 		resultado += ".\nUsted tiene obesidad de grado 2."
+		document.getElementById("res").style.backgroundColor = "orangered";
 
 	} else {
 		resultado += ".\nUsted tiene obesidad morbida."
+		document.getElementById("res").style.backgroundColor = "red";
 	}
 
 	console.log(resultado)
